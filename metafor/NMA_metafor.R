dafb8e9556e2cdc68e8d1ef49cf078dda21796af
Seth B Morgan <seth.b.morgan@gmail.com>
2023-09-13 23:02:18 -0400

    revising metafor code based on SK's responses 9_13_23

--- metafor/NMA_metafor.R
+++ metafor/NMA_metafor.R
@@ -37,26 +37,29 @@
 # Subset data following NMA analysis specifications: "Analysis Notes for Seth 9-1-2023
   
   ## Tabulate variables upon which to subset data
-  NMA_data %>% group_by(aggregated) %>% count() %>% ungroup()
-  NMA_data %>% group_by(measure_type) %>% count() %>% ungroup()
-  NMA_data %>% group_by(wwc_rating) %>% count() %>% ungroup()  
-  NMA_data %>% group_by(comparison_prelim, comparison_2) %>% count() %>% ungroup()
+  tabyl(NMA_data$aggregated)
+  tabyl(NMA_data$measure_type)
+  tabyl(NMA_data$wwc_rating)  
+  tabyl(NMA_data$intervention_prelim)    
+  tabyl(NMA_data$comparison_prelim) 
   
   ## Subset data for analysis 
-  NMA_data_analysis_subset <- NMA_data %>% filter(aggregated=="IN" & (measure_type=="Follow up (10-14 days)" | measure_type=="Main") & (wwc_rating=="MWR" | wwc_rating=="MWOR") & comparison_2=="BAU")
+  NMA_data_analysis_subset <- NMA_data %>% filter(aggregated=="IN" & (measure_type=="Follow up (10-14 days)" | measure_type=="Main") & (wwc_rating=="MWR" | wwc_rating=="MWOR") & comparison_prelim=="BAU" & (NL...48==1 | SE...50==1 | V...53==1 | F...56==1 | BX...63==1 | RS...74==1))
   
   ## Retabulate variables upon which to subset data to verify correct subset
-  NMA_data_analysis_subset %>% group_by(aggregated) %>% count() %>% ungroup()
-  NMA_data_analysis_subset %>% group_by(measure_type) %>% count() %>% ungroup()
-  NMA_data_analysis_subset %>% group_by(wwc_rating) %>% count() %>% ungroup()  
-  NMA_data_analysis_subset %>% group_by(comparison_2) %>% count() %>% ungroup()
+  tabyl(NMA_data_analysis_subset$aggregated)
+  tabyl(NMA_data_analysis_subset$measure_type)
+  tabyl(NMA_data_analysis_subset$wwc_rating)  
+  tabyl(NMA_data_analysis_subset$intervention_prelim)    
+  tabyl(NMA_data_analysis_subset$comparison_prelim) 
   
-  tabyl(NMA_data_analysis_subset$intervention) #NOTE: Need to add component bundle restriction: "ONLY be pulling in data if there is a 1 in the component columns (e.g., NL, V)" 
+  NMA_data_analysis_subset_check <- NMA_data_analysis_subset %>% select(intervention_prelim, NL...48, SE...50, V...53, F...56, BX...63, RS...74)
+  print(NMA_data_analysis_subset_check, n=Inf)
   
 # Create unique group ID for each independent group within a study (record ID)
   
   ##Keep only record ID, intervention/comparison bundle, intervention/comparison sample size 
-  NMA_data_grpID <- NMA_data_analysis_subset %>% select(record_id, intervention, comparison = comparison_2, intervention_n, comparison_n)
+  NMA_data_grpID <- NMA_data_analysis_subset %>% select(record_id, intervention = intervention_prelim, comparison = comparison_prelim, intervention_n, comparison_n)
   str(NMA_data_grpID)
   
   ## For each assignment group, combine component bundle and sample size into a single character variable to distinguish groups with the same sample sizes but different component bundles. 
@@ -101,29 +104,29 @@
 #   across those contrasts because they are the same unique group of individuals, otherwise we do not capture all dependencies.
   
   ## Merge unique group IDs onto the intervention groups
-  NMA_data_grpID_long_unique_withids_Imerge <- NMA_data_grpID_long_unique_withids %>% separate(bundle_samplesize, c("intervention","intervention_n"), "_")
+  NMA_data_grpID_long_unique_withids_Imerge <- NMA_data_grpID_long_unique_withids %>% separate(bundle_samplesize, c("intervention_prelim","intervention_n"), "_")
   NMA_data_grpID_long_unique_withids_Imerge
   NMA_data_grpID_long_unique_withids_Imerge$intervention_n <- as.numeric(NMA_data_grpID_long_unique_withids_Imerge$intervention_n)
   NMA_data_grpID_long_unique_withids_Imerge <- NMA_data_grpID_long_unique_withids_Imerge %>% rename(group1_id = group_id)
   str(NMA_data_grpID_long_unique_withids_Imerge)
-  NMA_data_analysis_subset$intervention <- NMA_data_analysis_subset$intervention %>% replace_na("NA") #This facilitates the merge below. When the IDs were created, intervention and intervention_n were combined then separated, which creates "NA" character values from the any original NA values, which don't match in a merge.
-  NMA_data_analysis_subset_grpID <- NMA_data_analysis_subset %>% left_join(NMA_data_grpID_long_unique_withids_Imerge, by = c("record_id","intervention","intervention_n"))
+  NMA_data_analysis_subset$intervention_prelim <- NMA_data_analysis_subset$intervention_prelim %>% replace_na("NA") #This facilitates the merge below. When the IDs were created, intervention and intervention_n were combined then separated, which creates "NA" character values from the any original NA values, which don't match in a merge.
+  NMA_data_analysis_subset_grpID <- NMA_data_analysis_subset %>% left_join(NMA_data_grpID_long_unique_withids_Imerge, by = c("record_id","intervention_prelim","intervention_n"))
   NMA_data_analysis_subset_grpID %>% group_by(group1_id) %>% count()
   
   ## Merge unique group IDs onto the comparison groups  
-  NMA_data_grpID_long_unique_withids_Cmerge <- NMA_data_grpID_long_unique_withids %>% separate(bundle_samplesize, c("comparison_2","comparison_n"), "_")  
+  NMA_data_grpID_long_unique_withids_Cmerge <- NMA_data_grpID_long_unique_withids %>% separate(bundle_samplesize, c("comparison_prelim","comparison_n"), "_")  
   NMA_data_grpID_long_unique_withids_Cmerge
   NMA_data_grpID_long_unique_withids_Cmerge$comparison_n <- as.numeric(NMA_data_grpID_long_unique_withids_Cmerge$comparison_n)
   NMA_data_grpID_long_unique_withids_Cmerge <- NMA_data_grpID_long_unique_withids_Cmerge %>% rename(group2_id = group_id)
   str(NMA_data_grpID_long_unique_withids_Cmerge)
-  NMA_data_analysis_subset$comparison_2 <- NMA_data_analysis_subset$comparison_2 %>% replace_na("NA") #This facilitates the merge below. When the IDs were created, comparison_2 and comparison were combined then separated, which creates "NA" character values from the any original NA values, which don't match in a merge.
-  NMA_data_analysis_subset_grpID <- NMA_data_analysis_subset_grpID %>% left_join(NMA_data_grpID_long_unique_withids_Cmerge, by = c("record_id","comparison_2","comparison_n"))
+  NMA_data_analysis_subset$comparison_prelim <- NMA_data_analysis_subset$comparison_prelim %>% replace_na("NA") #This facilitates the merge below. When the IDs were created, comparison_prelim and comparison were combined then separated, which creates "NA" character values from the any original NA values, which don't match in a merge.
+  NMA_data_analysis_subset_grpID <- NMA_data_analysis_subset_grpID %>% left_join(NMA_data_grpID_long_unique_withids_Cmerge, by = c("record_id","comparison_prelim","comparison_n"))
   NMA_data_analysis_subset_grpID %>% group_by(group2_id) %>% count()
   
   ## Validate merge results
   assert_values(NMA_data_analysis_subset_grpID, colnames= c("group1_id","group2_id"), test = "not_na", test_val = NA)
   assert_values(NMA_data_analysis_subset_grpID, colnames= "group1_id", test="not_equal", test_val= "group2_id")
-  NMA_data_analysis_subset_grpID_check <- NMA_data_analysis_subset_grpID %>% select(record_id, contrast_id, aggregated, measure_type, measure_name, wwc_rating, intervention, intervention_n, group1_id, comparison_2, comparison_n, group2_id)
+  NMA_data_analysis_subset_grpID_check <- NMA_data_analysis_subset_grpID %>% select(record_id, contrast_id, aggregated, measure_type, measure_name, wwc_rating, intervention_prelim, intervention_n, group1_id, comparison_prelim, comparison_n, group2_id)
   NMA_data_analysis_subset_grpID_check %>% print(n = Inf) 
 
 # calculate the variance-covariance matrix for multi-treatment studies
@@ -134,7 +137,7 @@
   NMA_data_analysis_subset_grpID <- contrmat(NMA_data_analysis_subset_grpID, grp1="group1_id", grp2="group2_id")
   NMA_data_analysis_subset_grpID
   
-# network meta-analysis using a contrast-based random-effects model using
+# execute network meta-analysis using a contrast-based random-effects model using
 # BAU as the reference condition (and by setting rho=0.60, tau^2
 # reflects the amount of heterogeneity for all treatment comparisons)
   class(NMA_data_analysis_subset_grpID$contrast_id) 
@@ -147,8 +150,13 @@
   tabyl(NMA_data_analysis_subset_grpID$dosage_weekly_freq)
   class(NMA_data_analysis_subset_grpID$dosage_weekly_freq)
   
-  res <- rma.mv(effect_size, var_covar_matrix, mods = ~ wwc_rating + dosage_weekly_freq - 1,
-                random = ~ contrast_id | record_id, rho=0.60, data=NMA_data_analysis_subset_grpID)
+  ##Run preliminary standard NMA without moderators but have placeholder code below set up for moderators
+  res <- rma.mv(effect_size, var_covar_matrix,
+          random = ~ intervention_prelim | record_id, rho=0.60, data=NMA_data_analysis_subset_grpID)
+  
+  #res <- rma.mv(effect_size, var_covar_matrix, mods = ~ wwc_rating + dosage_weekly_freq - 1,
+          #random = ~ contrast_id | record_id, rho=0.60, data=NMA_data_analysis_subset_grpID)
+  
   res
   
   weights.rma.mv(res)
