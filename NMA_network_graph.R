08329c82d57e626a3c77bf3260645af93ca044f9
Seth B Morgan <seth.b.morgan@gmail.com>
2024-03-06 16:32:31 -0500

    Update NMA_network_graph.R

--- NMA_network_graph.R
+++ NMA_network_graph.R
@@ -5,17 +5,18 @@
 # allows for the ranking of interventions.  
 
 # Load required packages
-  
+
   ## Install 'devel' version of metafor package
   ##install.packages("remotes") 
   ##remotes::install_github("wviechtb/metafor") 
   
   ## Install and load other required packages
   ##install.packages("pacman") 
-  pacman::p_load(metafor, igraph, network, tidyr)
+  pacman::p_load(metafor, googlesheets4, igraph, network, tidyr, dplyr, janitor, assertable)
 
-# Load (read) data (i.e., copy data to 'dat')
-  #dat <- read_sheet("https://docs.google.com/spreadsheets/d/1bWugw06yFyetIVYlhAHHzM_d3KGhegxxLBm-5463j2Q/edit#gid=0") #Test data
+# Load and subset data
+  
+  ##Load (read in) data
   NMA_data <- read_sheet("https://docs.google.com/spreadsheets/d/1cv5ftm6-XV28pZ_mN43K7HH3C7WhsPMnPsB1HDuRLE4/edit#gid=0") #Full data set
   NMA_data %>% count()
   tabyl(NMA_data$record_id)
@@ -27,39 +28,56 @@
   NMA_data_analysis_subset <- subset(NMA_data, (measure_type=="Main" | measure_type=="Follow Up (10-14 Days)") &
                                        aggregated=="IN" & (wwc_rating=="MWOR" | wwc_rating=="MWR") &
                                        comparison_prelim=="BAU" & (NL_TX==1 | SE_TX==1 | VF_TX==1 | F_TX==1 | BX_TX==1 | RS_TX==1 )) 
-  
+
 # Create network graph
   
   ## Use the igraph package
-    ### Note: The data are currently in a contrast-based, "wide" format in which each contrast is an observation 
-    ###       (i.e., intervention and comparison assignments of each contrast are in separate columns of the same row.)
-    ###       Need to reshape the data "long" to an arm-based format in which the two assignment groups (intervention/comparison)
-    ###       are in separate rows of the same column grouped by contrast ID across those rows. This facilitates the pairing of intervention/comparison 
-    ###       groups for the creation of the (weighted) edges of the network graph.
-  NMA_data_analysis_subset2 <- NMA_data_analysis_subset %>% select(record_id, contrast_id, intervention_prelim, comparison_prelim, domain, measure_name, es_id, effect_size)
-  print(NMA_data_analysis_subset2) #Example rows of the contrast-based wide format. Compare to the long format printed below.
   
-  NMA_data_analysis_subset3 <- NMA_data_analysis_subset %>% distinct(record_id, contrast_id, .keep_all = TRUE)
-  
-  NMA_data_analysis_subset_long <- pivot_longer(NMA_data_analysis_subset, c(intervention_prelim, comparison_prelim), names_to = "assignment_I_C", values_to = "intervention_comparison")
-  
-  NMA_data_analysis_subset_long2 <- NMA_data_analysis_subset_long %>% select(record_id, contrast_id, assignment_I_C, intervention_comparison, domain, measure_name, es_id, effect_size)
-  print(NMA_data_analysis_subset_long2) #Example rows of the arm-based long format. Compare to the wide format printed above.
-  
-  dat_igraph <- NMA_data_analysis_subset_long
-  dat_igraph$contrast_id <- as.character(dat_igraph$contrast_id)
-  pairs <- data.frame(do.call(rbind, sapply(split(dat_igraph$intervention_comparison, dat_igraph$contrast_id), function(x) t(combn(x,2)))), stringsAsFactors=FALSE)
-  print(pairs)
-  pairs$X1 <- factor(pairs$X1, levels=sort(unique(dat_igraph$intervention_comparison)))
-  pairs$X2 <- factor(pairs$X2, levels=sort(unique(dat_igraph$intervention_comparison)))
-  tab <- table(pairs[,1], pairs[,2])
-  tab
-  
-  g <- graph_from_adjacency_matrix(tab, mode = "plus", weighted=TRUE, diag=FALSE)
-  plot(g, edge.curved=FALSE, edge.width=E(g)$weight/20,
-       layout=layout_in_circle(g),
-       #layout=layout_nicely(g),
-       #layout=layout_with_lgl(g),
-       vertex.size=20, vertex.color="lightgray", vertex.label.color="black", vertex.label.font=2)  
-  
+  ### Note: The data are currently in a contrast-based, "wide" format in which each contrast is an observation 
+  ###       (i.e., intervention and comparison assignments of each contrast are in separate columns of the same row.)
+  ###       Need to reshape the data "long" to an arm-based format in which the two assignment groups (intervention/comparison)
+  ###       are in separate rows of the same column grouped by contrast ID across those rows. This facilitates the pairing of intervention/comparison 
+  ###       groups for the creation of the (weighted) edges of the network graph.
+    
+    ###Review data in contrast-based wide format before reshape for comparison with data after reshape (as a desk check)
+    NMA_data_analysis_subset$contrast_id <- as.character(NMA_data_analysis_subset$contrast_id)
+    NMA_data_analysis_subset2 <- NMA_data_analysis_subset %>% select(record_id, contrast_id, intervention_prelim, comparison_prelim, domain, measure_name, es_id, effect_size)
+    print(NMA_data_analysis_subset2) #Example rows of the contrast-based wide format. Compare to the long format printed below.
+    
+    ###Each unique contrast within each unique study should only be counted once when weighting the network connections between each unique contrast combination of intervention and comparison in the network graph.
+    ###Because there can be multiple measures within multiple domains within each unique contrast within each unique study, we need reduce the data set to one observation per unique contrast within each unique study 
+    ### so that a contrast with more domains/measures than another contrast is not overweighted in the visualization of the network connections (re the relative thicknesses of the "edges" between the I/C nodes in the network graph).
+    NMA_data_analysis_subset3 <- NMA_data_analysis_subset %>% distinct(record_id, contrast_id, .keep_all = TRUE)
+    NMA_data_analysis_subset4 <- NMA_data_analysis_subset3 %>% select(record_id, contrast_id, intervention_prelim, comparison_prelim, domain, measure_name, es_id, effect_size)
+    NMA_data_analysis_subset4 <- NMA_data_analysis_subset4 %>% unite("int_comp_prelim" , c(intervention_prelim,comparison_prelim), remove = FALSE)
+    tabyl(NMA_data_analysis_subset4$int_comp_prelim) #The relative number of observations (n) for the intervention_BAU contrasts in this table should match the relative thicknesses of the lines (called "edges") in the network graph.
+    
+    ###Reshape data to arm-based long format
+    NMA_data_analysis_subset_long <- pivot_longer(NMA_data_analysis_subset3, c(intervention_prelim, comparison_prelim), names_to = "assignment_I_C", values_to = "intervention_comparison")
+    
+    ###Review data in arm-based long format after reshape for comparison with data before reshape (as a desk check)
+    NMA_data_analysis_subset_long2 <- NMA_data_analysis_subset_long %>% select(record_id, contrast_id, assignment_I_C, intervention_comparison, domain, measure_name, es_id, effect_size)
+    print(NMA_data_analysis_subset_long2) #Example rows of the arm-based long format. Compare to the wide format printed above.
+    
+    ###Create the table of intervention/comparison pairs for creating the network graph with igraph
+    dat_igraph <- NMA_data_analysis_subset_long
+    dat_igraph$contrast_id <- as.character(dat_igraph$contrast_id)
+    pairs <- data.frame(do.call(rbind, lapply(split(dat_igraph$intervention_comparison, dat_igraph$contrast_id), function(x) t(combn(x,2)))), stringsAsFactors=FALSE)
+    print(pairs)
+    pairs$X1 <- factor(pairs$X1, levels=sort(unique(dat_igraph$intervention_comparison)))
+    pairs$X2 <- factor(pairs$X2, levels=sort(unique(dat_igraph$intervention_comparison)))
+    tab <- table(pairs[,1], pairs[,2])
+    tab
+    
+    ###Creating the network graph with igraph
+    set.seed(3524)
+    g <- graph_from_adjacency_matrix(tab, mode = "plus", weighted=TRUE, diag=FALSE)
+    plot(g, edge.curved=FALSE, edge.width=E(g)$weight,
+         layout=layout_in_circle(g),
+         #layout=layout_nicely(g),
+         #layout=layout_with_lgl(g),
+         vertex.size=20, vertex.color="lightgray", vertex.label.color="black", vertex.label.font=2)  
+
   ## Use the network package
+  
+  ### Note: Creating the network graph with igraph seems to work just fine. No need currently to adapt using the network package.
